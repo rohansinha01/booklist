e796b32e7ca599802818b12fab9e627b4a628486
rohansinha01 <rohansinha01@gmail.com>
2023-12-23 11:26:23 -0500

    updated and delete options created

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require("dotenv").config() // this is how we make use of our .env variables
 require("./config/db")
 const express = require("express")
 const morgan = require("morgan") // logger
+const methodOverride = require("method-override")
 
 const app = express()
 const { PORT = 3013 } = process.env;
@@ -18,6 +19,7 @@ const Book = require("./models/Book")
  */
 app.use(morgan("dev")) // logging
 app.use(express.urlencoded({ extended: true })) // body parser this is how we get access to req.body
+app.use(methodOverride("_method")) // Lets us use DELETE PUT HTTP verbs
 /**
  * Routes & Router
  */
@@ -31,6 +33,40 @@ app.get("/books", async (req, res) => {
         books: books.reverse()
     })
    
+})
+
+// DELETE
+app.delete("/books/:id", async (req, res) => {
+    try {
+    // Find a ook and then delete
+    let deletedBook = await Book.findByIdAndDelete(req.params.id)
+    console.log(deletedBook)
+    // Redirect back to the index
+    res.redirect("/books")
+    } catch (error) {
+        res.status(500).send("something went wrong when deleting")
+    }
+})
+
+// UPDATE
+app.put("/books/:id", async (req, res) => {
+    // handle our checkbox
+    if (req.body.completed === "on") {
+        req.body.completed = true
+    } else {
+        req.body.completed = false
+    }
+    // Then find by id and update with the req.body
+    // findByIDAndUpdate - id , data to update, options
+    let updatedBook = await Book.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+                new: true
+        }
+    )
+    // redirect to the show route with the updated book
+    res.redirect(`/books/${updatedBook._id}`)
 })
 
 // NEW - GET for the form to create a new book
@@ -58,7 +94,30 @@ app.post("/books", async (req, res) => {
     }
 })
 
-// SHOW - GET rendering only one book
+// EDIT
+app.get("/books/edit/:id", async (req, res) => {
+    try {
+        // find the book to edit
+        let foundBook = await Book.findById(req.params.id)
+        res.render("edit.ejs", {
+            book: foundBook
+        })
+    } catch (error) {
+        res.send("hello from the error")
+    }
+})
+
+// Show - GET rendering only one book
+app.get("/books/:id", async (req, res) => {
+    // find a book by _id
+    // let foundBook = await Book.findById(req.params.id) // the request params object
+
+   
+    // render show.ejs with the foundBook
+    res.render("show.ejs", {
+        book: await Book.findById(req.params.id)
+    })
+})
 
 /**
  * Server Listener
